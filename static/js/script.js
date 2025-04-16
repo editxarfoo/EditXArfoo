342f44485cfae169388277c038079a0039daaf5d
editxarfoo <edit.x.arfoo@gmail.com>
2025-04-16 10:19:12 +0600

    Added Dark Mode Toggle feature

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,8 @@
 function toggleDarkMode() {
   document.body.classList.toggle("dark-mode");
 }
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+  const btn = document.getElementById("darkModeBtn");
+  btn.innerText = document.body.classList.contains("dark-mode") ? "Light Mode" : "Dark Mode";
+}
